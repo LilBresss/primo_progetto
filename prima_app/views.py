ad83bbaef41ec29dd6e36bd67843edcc80873485
Bress <simobress2007@gmail.com>
2025-10-27 13:12:40 +0100

    Passo 9 : add index generale

--- prima_app/views.py
+++ prima_app/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request,"homepage.html")
+    return render(request,"prima_app/homepage.html")
 
 def welcome(request):
-    return render(request,"welcome.html")
+    return render(request,"prima_app/welcome.html")
 
 def lista(request):
-    return render(request,"lista.html")
+    return render(request,"prima_app/lista.html")
 
 def chi_siamo(request):
-    return render(request,"chi_siamo.html")
+    return render(request,"prima_app/chi_siamo.html")
+
+def variabili(request):
+    context= {
+        'var1': 'Prima variabile',
+        'var2': 'Seconda variabile',
+        'var3': 'Terza variabile'
+    }
+    return render(request,"prima_app/variabili.html",context)
+
+def index(request):
+    return render(request,"index.html")

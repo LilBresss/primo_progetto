770897b1553e7a4cdaf3f247930cec94a7ea5eef
Bress <simobress2007@gmail.com>
2026-01-12 13:52:35 +0100

    Creazione App News (Parte 2)

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, articoloDetailView
+from .views import home, articoloDetailView, listaArticoli
 
 app_name="news"
 urlpatterns = [
     path('news', home, name='homeview'),
-    path("news/articoli/<int:pk>", articoloDetailView, name="articoli_detail")
+    path("news/articoli/<int:pk>", articoloDetailView, name="articoli_detail"),
+    path("news/lista_articoli/<int:pk>", listaArticoli, name="lista_articoli_giornalista"),
+    path("news/lista_articoli", listaArticoli, name="lista_articoli")
 ]
--- news/views.py
+++ news/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render, get_object_or_404
 from .models import Articolo, Giornalista
 
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
 def home(request):
     articoli = Articolo.objects.all()
     giornalisti = Giornalista.objects.all()
-
-    context = {"articoli":articoli, "giornalisti":giornalisti}
-    
+    context = {
+        "articoli":articoli, "giornalisti":giornalisti
+    }
     print(context)
-
     return render(request, "home.html", context)
 
 def articoloDetailView(request, pk):
     articolo = get_object_or_404(Articolo, pk=pk)
-    context = {"articolo": articolo}
+    context = {
+        "articolo": articolo
+    }
     return render(request, "articolo_detail.html", context)
+
+def listaArticoli(request, pk=None):
+    if(pk==None):
+        articoli = Articolo.objects.all()
+    else:
+        articoli = Articolo.objects.filter(giornalista_id=pk)
+    context = {
+        'articoli': articoli
+    }
+    return render(request, 'lista_articoli.html', context)
